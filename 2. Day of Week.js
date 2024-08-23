fe3552e1e911736f22e05d734cd4ba86286a3bf7
Miroslav Tsarev <mtzarev@gmail.com>
2024-08-23 22:54:21 +0300

    Add files via upload

---
+++ 2. Day of Week.js
@@ -0,0 +1,15 @@
+function dayOfWeek(num) {
+    let day = "";
+    switch (num) {
+        case 1: day = "Monday"; break;
+        case 2: day = "Tuesday"; break;
+        case 3: day = "Wednesday"; break;
+        case 4: day = "Thursday"; break;
+        case 5: day = "Friday"; break;
+        case 6: day = "Saturday"; break;
+        case 7: day = "Sunday"; break;
+        default: day = "Invalid day!";
+    }
+    console.log(day);
+}
+dayOfWeek(7);
